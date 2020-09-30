0588140b670707542f0f6c4f01f04ab072ea33bd
hyp3 <0xshb@protonmail.com>
2020-09-30 18:50:15 +0300

    added basic csv more to do

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 import os 
+import csv 
 
-check_dir = os.path.isdir("databases")
-if check_dir:
-    print("Folder Found")
+# create new csv file 
+def add():
+    login_name = input("Enter the name of the login name: ")
+    username = input("Enter the username: ")
+    email = input("Enter the email: ")
+    passowrd = input("Enter the passowrd: ")
+    notes = input("Any Additional notes?: ")
+    new_row = [[login_name, username, email, passowrd, notes]]
+    return new_row
 
-else: 
-    os.mkdir("databases")
-    print("Created New Directory!")
+def check():
+    choice_1 = input("1 : New Item. \t2 : Read Item: ")
+    if  choice_1 == 1:
+        return add()
+
+    
+
+
+
+new_csv_name = input("Enter the new Database name: ")
+new_csv_name = new_csv_name + ".csv"
+with open (new_csv_name, 'w') as new_file:
+    writer = csv.writer(new_file)
+    x = add()
+    writer.writerows(x)
+        
+
 
 
 
