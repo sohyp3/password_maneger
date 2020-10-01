5a9c3e9c8168133a95821add5fc2c96af02d54c0
hyp3 <0xshb@protonmail.com>
2020-10-01 23:34:12 +0300

    ..

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os 
+import csv 
 
+new_csv_name = "db.csv"
 # check for the dir 
 # currently disabled because the multiple databases feature will be added later! 
 
@@ -16,5 +18,6 @@ check_db = os.path.isfile("db.csv")
 if check_db:
     print("found welcome")
 else: 
-    
+    with open(new_csv_name, 'w') as newFile:
+        newFileWriter = csv.writer(newFile)    
     print("Just Created one!")
