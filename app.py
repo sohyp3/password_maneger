50dc6b2f0af65cf272d15ec82555d7fd7a6c7158
hyp3 <0xshb@protonmail.com>
2020-10-01 23:30:11 +0300

    ..

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os 
 
-check_dir = os.path.isdir("databases")
-if check_dir:
-    print("Folder Found")
+# check for the dir 
+# currently disabled because the multiple databases feature will be added later! 
 
+# check_dir = os.path.isdir("databases")
+# if check_dir:
+#     print("Folder Found")
+
+# else: 
+#     os.mkdir("databases")
+#     print("Created New Directory!")
+
+
+check_db = os.path.isfile("db.csv")
+if check_db:
+    print("found welcome")
 else: 
-    os.mkdir("databases")
-    print("Created New Directory!")
-
-
-
+    
+    print("Just Created one!")
