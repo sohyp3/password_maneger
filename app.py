713d516e416154568df98712a7295dd1dd3cd12c
hyp3 <0xshb@protonmail.com>
2020-10-05 20:56:27 +0300

    made the edit

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ new_csv_name = "db.csv"
 # create new row to add 
 class operations():
     def __init__(self):
-        self.choice = int(input("1 : New Item. \t2 : Search for Item: "))
+        self.choice = int(input("1 : New Item. \t2 : Search for Item \t3 : for edit  "))
         if self.choice == 1:
             self.add()
         elif self.choice == 2: 
             self.search()
+        elif self.choice ==3:
+            self.edit()
         else:
             print("BRoKEn!")
 
@@ -27,8 +29,8 @@ class operations():
             newFileWriter.writerow(self.new_row)
 
     def remove(self):
-        self.remove_item = input("Please Enter the item you want to remove: ")
-        
+        self.remove_item = input("Enter the Login name that you want to delete: ")
+
 
     def search(self):
         self.search_for = input("Tell me What are you looking for? ")
@@ -42,6 +44,54 @@ class operations():
                     for specific_search in word:
                         if self.search_for in specific_search:
                             print(word)
+    
+    def edit(self):
+        with open(new_csv_name ,"r") as newFile:
+            readie = csv.reader(newFile)
+            for word in readie:
+                print(word)
+
+        self.edit_file = input ("Which login you want to edit? ")
+
+        with open (new_csv_name ,"r")as newFile:
+            readie = csv.reader(newFile)
+            
+            with open ("temp.csv","w") as newFile2:
+                writie = csv.writer(newFile2)
+                
+
+                for  word in readie:
+                    if self.edit_file in word[0]:
+                        print(word)
+                        are_you_sure = input("are you sure?? (Y/n)").lower
+                        if are_you_sure == "n":
+                            break
+                        
+
+                        test_list = ["reddit","yollo", "mollo@noonelikesyou.com", "ferystrongpasswerd",""]
+                        
+                        writie.writerow(test_list)
+                    else: 
+                        writie.writerow(word)
+
+        with open (new_csv_name, "w") as newFile3:
+            writie = csv.writer(newFile3)
+            with open ("temp.csv","r") as newFile2:
+                readie = csv.reader(newFile2)
+
+                for word in readie:
+                    writie.writerow(word)
+                
+
+
+
+
+                    
+
+
+
+
+
 
 
 
